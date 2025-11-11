7a74c8e4c6ca80d04eedc8ad737cf311662fcd5f
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2025-11-11 21:30:58 +0000

    Added moving toggle nav bar, need to fix navbar in widescreen just being a column

--- header.js
+++ header.js
@@ -1,14 +1,28 @@
 var style = window.getComputedStyle(document.body);
 
+window.onLoad(setupHeader());
+
+function setupHeader(){
+  if (window.innerWidth > 300){
+    document.querySelector('header .nav').classList.add('show-nav');
+    document.querySelector('header .nav').classList.remove('hidden-nav');
+  }
+  else {
+    document.querySelector('header .nav').classList.add('hidden-nav');
+    document.querySelector('header .nav').classList.remove('show-nav');
+  }
+}
+
 function toggleNav(){
   const nav = document.querySelector('header .nav');
   let displayStyle = window.getComputedStyle(nav).display;
   nav.classList.toggle('hidden-nav');
+  nav.classList.toggle('show-nav');
 }
 
 function onResize(){
   const nav = document.querySelector('header .nav');
-  if (window.innerWidth >= 300){
+  if (window.innerWidth > 300){
     nav.classList.remove('hidden-nav');
     nav.classList.add('show-nav');
   }
